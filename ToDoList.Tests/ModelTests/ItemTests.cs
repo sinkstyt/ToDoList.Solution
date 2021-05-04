a6eb0040b3d1404027372e3fd04a79761d83c25e
Tyler Sinks <tyler.sinks@gmail.com>
2021-05-03 23:04:04 -0400

    pass Id on Item spec
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>
    Co-Authored by Salim Mayan <mailsalim@gmail.com>

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ToDoList.Models;
+using System;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+    public void Dispose()
+    {
+      // Item.ClearAll();
+    }
+
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      int result = newItem.Id;
+      //   int result = 1;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+  public void Find_ReturnsCorrectItem_Item()
+  {
+    //Arrange
+    string description01 = "Walk the dog";
+    string description02 = "Wash the dishes";
+    Item newItem1 = new Item(description01);
+    Item newItem2 = new Item(description02);
+
+    //Act
+    Item result = Item.Find(2);
+
+    //Assert
+    Assert.AreEqual(newItem2, result);
+  }
+  }
+}
