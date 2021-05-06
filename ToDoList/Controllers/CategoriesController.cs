9785d1213fe83962e3f6fbbcabfb2515fce17653
Tyler Sinks <tyler.sinks@gmail.com>
2021-05-05 23:39:19 -0400

    add many new routes
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>
    Co-Authored by Salim Mayan <mailsalim@gmail.com>

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+
+
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+
+    [HttpPost("/categories")]
+    public ActionResult Create(string categoryName)
+    {
+      Category newCategory = new Category(categoryName);
+      return RedirectToAction("Index");
+    }
+    [HttpPost("/categories/{categoryId}/items")]
+    public ActionResult Create(int categoryId, string itemDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category foundCategory = Category.Find(categoryId);
+      Item newItem = new Item(itemDescription);
+      foundCategory.AddItem(newItem);
+      List<Item> categoryItems = foundCategory.Items;
+      model.Add("items", categoryItems);
+      model.Add("category", foundCategory);
+      return View("Show", model);
+    }
+    [HttpGet("/categories/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category selectedCategory = Category.Find(id);
+      List<Item> categoryItems = selectedCategory.Items;
+      model.Add("category", selectedCategory);
+      model.Add("items", categoryItems);
+      return View(model);
+    }
+  }
+}
